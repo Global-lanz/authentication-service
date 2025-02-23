917d0677a78439dbf45dddfbfd5ae8ee74058a53
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-02-23 12:49:45 +0000

    Added new changes
    I have added new changes to it

--- src/main/java/lanz/global/authenticationservice/api/UserApi.java
+++ src/main/java/lanz/global/authenticationservice/api/UserApi.java
@@ -1,5 +1,8 @@
 package lanz.global.authenticationservice.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lanz.global.authenticationservice.api.request.user.LoginRequest;
 import lanz.global.authenticationservice.api.request.user.RegistrationRequest;
@@ -26,32 +29,43 @@ import java.util.UUID;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserApi {
 
-    private final UserService userService;
-    private final CompanyService companyService;
+	private final UserService userService;
+	private final CompanyService companyService;
 
-    @PostMapping("/register")
-    public ResponseEntity<Void> register(@Valid @RequestBody RegistrationRequest request) throws Exception {
-        UUID userId = userService.register(request);
-        return ResponseEntity.created(URI.create("/user/" + userId)).build();
-    }
+	@PostMapping("/register")
+	@ApiResponse(responseCode = "200", description = "User has been created")
+	@ApiResponse(responseCode = "404", description = "Bad request")
+	public ResponseEntity<Void> register(@Valid @RequestBody RegistrationRequest request) throws Exception {
+		UUID userId = userService.register(request);
+		return ResponseEntity.created(URI.create("/user/" + userId)).build();
+	}
 
-    @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
-        LoginResponse response = new LoginResponse(userService.login(request));
-        return ResponseEntity.ok(response);
-    }
+	@PostMapping("/login")
+	@Operation(summary = "Authentication", description = "The endpoint for user authentication")
+	@ApiResponse(responseCode = "200")
+	@ApiResponse(responseCode = "401", description = "Invalid username or password provided")
+	public ResponseEntity<LoginResponse> login(
+			@Schema(description = "The request body") @Valid @RequestBody LoginRequest request) {
+		LoginResponse response = new LoginResponse(userService.login(request));
+		return ResponseEntity.ok(response);
+	}
 
-    @GetMapping("/currency")
-    public ResponseEntity<List<CurrencyResponse>> findCurrencies() {
-        List<CurrencyResponse> currencies = companyService.findAllCurrencies().stream().map(
-                currency -> new CurrencyResponse(currency.getCurrencyId(), currency.getName(), currency.getSymbol(), currency.getCode())
-        ).toList();
-        return ResponseEntity.ok(currencies);
-    }
+	@GetMapping("/currency")
+	@Operation(summary = "Currencies", description = "The endpoint for retrieving the list of currencies")
+	@ApiResponse(responseCode = "200")
+	public ResponseEntity<List<CurrencyResponse>> findCurrencies() {
+		List<CurrencyResponse> currencies = companyService.findAllCurrencies().stream().map(
+				currency -> new CurrencyResponse(currency.getCurrencyId(), currency.getName(), currency.getSymbol(), currency.getCode())
+		).toList();
+		return ResponseEntity.ok(currencies);
+	}
 
-    @GetMapping("/user")
-    public ResponseEntity<UserAccountResponse> getUserAccountData() {
-        return ResponseEntity.ok(new UserAccountResponse(userService.getUserAccount()));
-    }
+	@GetMapping("/user")
+	@Operation(summary = "User account data", description = "The endpoint for retrieving user account data")
+	@ApiResponse(responseCode = "200")
+	@ApiResponse(responseCode = "401", description = "The user is not authenticated")
+	public ResponseEntity<UserAccountResponse> getUserAccountData() {
+		return ResponseEntity.ok(new UserAccountResponse(userService.getUserAccount()));
+	}
 
 }
--- src/main/java/lanz/global/authenticationservice/api/request/user/RegistrationRequest.java
+++ src/main/java/lanz/global/authenticationservice/api/request/user/RegistrationRequest.java
@@ -1,18 +1,19 @@
 package lanz.global.authenticationservice.api.request.user;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 import java.io.Serializable;
 import java.util.UUID;
 
-public record RegistrationRequest(@NotBlank String name,
-                                  @NotBlank String email,
-                                  @NotBlank String password,
-                                  @NotBlank String confirmPassword,
-                                  @NotBlank String companyName,
-                                  UUID currencyId,
-                                  String country
+public record RegistrationRequest(@Schema(description = "The name of the user") @NotBlank String name,
+								  @Schema(description = "The email of the user") @NotBlank String email,
+								  @Schema(description = "The password of the user") @NotBlank String password,
+								  @Schema(description = "The confirm password that should match with the password") @NotBlank String confirmPassword,
+								  @Schema(description = "The name of the company") @NotBlank String companyName,
+								  @Schema(description = "The default currency of the company") UUID currencyId,
+								  @Schema(description = "The country of the user") String country
 ) implements Serializable {
 
 
