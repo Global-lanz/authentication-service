d8f7427d2c4050b9ed4ac214bf96ec171bab5c4b
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-04-14 21:07:33 +0100

    Created rules for contract

---
+++ src/main/resources/db/migration/V3__1.4.0_Contract_rules.sql
@@ -0,0 +1,6 @@
+INSERT INTO rule (name, description) VALUES ('CREATE_CONTRACT', 'rule.create-contract.description');
+INSERT INTO rule (name, description) VALUES ('UPDATE_CONTRACT', 'rule.update-contract.description');
+INSERT INTO rule (name, description) VALUES ('LIST_CONTRACTS', 'rule.list-contracts.description');
+INSERT INTO rule (name, description) VALUES ('GET_CONTRACT', 'rule.get-contract.description');
+INSERT INTO rule (name, description) VALUES ('DELETE_CONTRACT', 'rule.delete-contract.description');
+INSERT INTO rule (name, description) VALUES ('UPDATE_CONTRACT_STATUS', 'rule.update-contract-status.description');
