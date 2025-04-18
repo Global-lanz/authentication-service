b0e4adc89a64e85a549a624cde0b1cde4209f660
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-04-18 16:05:10 +0100

    Added invoice rules

---
+++ src/main/resources/db/migration/V4__1.5.0_Invoice_rules.sql
@@ -0,0 +1,10 @@
+INSERT INTO rule (name, description) VALUES ('UPDATE_PAYMENT', 'rule.update-payment.description');
+INSERT INTO rule (name, description) VALUES ('GET_PAYMENT', 'rule.get-payment.description');
+INSERT INTO rule (name, description) VALUES ('LIST_PAYMENTS', 'rule.list-payments.description');
+INSERT INTO rule (name, description) VALUES ('DELETE_PAYMENT', 'rule.delete-payment.description');
+
+INSERT INTO rule (name, description) VALUES ('CREATE_INVOICE', 'rule.create-invoice.description');
+INSERT INTO rule (name, description) VALUES ('GET_INVOICE', 'rule.get-invoice.description');
+INSERT INTO rule (name, description) VALUES ('UPDATE_INVOICE', 'rule.update-invoice.description');
+INSERT INTO rule (name, description) VALUES ('DELETE_INVOICE', 'rule.delete-invoice.description');
+INSERT INTO rule (name, description) VALUES ('LIST_INVOICES', 'rule.list-invoice.description');
