cfc32f2734e6354c9924faf3fed7a6faa1ecba45
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-05-02 10:35:39 +0100

    SCRUM-34 / Fixed endpoint activation to not change password if not provded (#20)

--- src/main/java/lanz/global/authenticationservice/exception/UserAlreadyExistsException.java
+++ src/main/java/lanz/global/authenticationservice/exception/UserAlreadyExistsException.java
@@ -1,6 +1,11 @@
 package lanz.global.authenticationservice.exception;
 
+import java.io.Serial;
+
 public class UserAlreadyExistsException extends BadRequestException {
+
+    @Serial
+    private static final long serialVersionUID = -6790788189994100246L;
 
     public UserAlreadyExistsException(String email) {
         super("exception.user-already-exists.title", "exception.user-already-exists.message", email);
